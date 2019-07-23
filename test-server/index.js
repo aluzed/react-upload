eddcff898d217868c69f34afe756796fbf2ac360
aluzed <alexandre.penombre@gmail.com>
2019-07-23 17:49:00 +0200

    audit fix

--- test-server/index.js
+++ test-server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/uploads', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/uploads', { useNewUrlParser: true });
 
 
 const upload = multer({ dest: `./uploads/` });
